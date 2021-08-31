010b528cdfcebe2008fec4bfcc6666cf4e584635
SebastianOpiyo <sebamopale@gmail.com>
2021-08-31 13:59:48 +0300

    Add categories.

--- Chuck_Norris/chuck-norris/src/components/jokescategories.js
+++ Chuck_Norris/chuck-norris/src/components/jokescategories.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
-return(
-        <div>
-            <h1>Categories</h1>
-        </div>
-    );
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.chucknorris.io/jokes/categories")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setCategories(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+      }, [])
+
+      if (error) {
+        return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <ul>
+                        {categories.map(category => (
+                        <li>
+                            <Link to={`/categories/${category.id}`}>{category}</Link>
+                        </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
 }
 export default Categories;
